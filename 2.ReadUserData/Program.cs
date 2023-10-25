658139db4a9b950dca0621af9f310bebd5b0f0f2
Homayoon Parsaei <Homayoon.parsaei17@gmail.com>
2023-10-25 10:45:01 -0500

    Read User Data

---
+++ 2.ReadUserData/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _2.ReadUserData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read first name
+            Console.WriteLine("What is your first name ?");
+            String FirstName = Console.ReadLine();
+            //Read first name
+            Console.WriteLine("What is your last name ?");
+            String LastName = Console.ReadLine();
+            //Read Age
+            Console.WriteLine("How old are you ?");
+            String  Age = Console.ReadLine();
+            //Read phone number
+            Console.WriteLine("What is your phone number?");
+            String PhoneNUmber = Console.ReadLine();
+            //Read ID number
+            Console.WriteLine("What is your ID number?");
+            String IDNumber = Console.ReadLine();
+            //Output
+            Console.WriteLine($"Your name is {FirstName} {LastName} and you are {Age} years old.");
+            Console.WriteLine($"Your phone number is: {PhoneNUmber} and your ID number is: {IDNumber}.");
+        }
+    }
+}
