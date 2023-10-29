400b4078ab4e736a39d2750143e50b3f25f1b64e
Homayoon Parsaei <Homayoon.parsaei17@gmail.com>
2023-10-29 01:03:35 -0500

    #

--- 4.ConsoleMethodAndProperty/Program.cs
+++ 4.ConsoleMethodAndProperty/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace _4.ConsoleMethodAndProperty
+﻿using System;
+namespace _4.ConsoleMethodAndProperty
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+            Console.SetBufferSize(500, 500);
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Title = "My Console!";
+            Console.SetCursorPosition(0, 3);
+            Console.WriteLine($"Changing the Console attributes\nForground: Black\nBackground: White\nTitle: My Console\nCursorPosition: 0,3");
+            Console.WriteLine("Write");
         }
     }
 }
---
+++ 5.StringMethodAndProperty/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _5.StringMethodAndProperty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String Phonenumber = "+989223334444";
+            String PhoneNumber = Console.ReadLine();
+            Str
+
+
+
+        }
+    }
+}
