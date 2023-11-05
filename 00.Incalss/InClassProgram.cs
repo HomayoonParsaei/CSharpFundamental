68729194d408994b9a6ddf6ec8db76b34bd09dff
Homayoon Parsaei <Homayoon.parsaei17@gmail.com>
2023-11-05 09:28:00 -0600

    try catch

--- 00.Incalss/InClassProgram.cs
+++ 00.Incalss/InClassProgram.cs
@@ -6,30 +6,47 @@
 using Microsoft.VisualBasic;
 using System.ComponentModel.Design;
 using System.Globalization;
+using System.Linq.Expressions;
 
 namespace _00.Incalss
 {
-  
+
 
     //In class test
+
+
     internal class InClassProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your gender Male(M)/Female(F) please :");
-            string genderStr = Console.ReadLine();
 
-            Console.WriteLine("How old are you?");
-            int age = int.Parse(Console.ReadLine());
-            
-            
-            //genderIsMale = (!string.IsNullOrEmpty(genderStr) && (genderStr== "Male" || genderStr == "male" || genderStr == "M" || genderStr == "m")) ?true: false;
-           bool genderIsMale = (!string.IsNullOrEmpty(genderStr) && (genderStr.ToLower()=="male" || genderStr.ToLower()=="m")) ?true: false;
-           
-            if (genderIsMale)
-                Console.WriteLine($"your gender is Male(M) and you are {age} years old.");
-            else
-                Console.WriteLine("your gender is not Male(M).");
+            try
+            {
+                    Console.WriteLine("Enter your gender Male(M)/Female(F) please :");
+                    string genderStr = Console.ReadLine();
+
+                    Console.WriteLine("How old are you?");
+                    int age = int.Parse(Console.ReadLine());
+
+
+                    //genderIsMale = (!string.IsNullOrEmpty(genderStr) && (genderStr== "Male" || genderStr == "male" || genderStr == "M" || genderStr == "m")) ?true: false;
+                    bool genderIsMale = (!string.IsNullOrEmpty(genderStr) && (genderStr.ToLower() == "male" || genderStr.ToLower() == "m")) ? true : false;
+
+                    if (genderIsMale)
+                        Console.WriteLine($"your gender is Male(M) and you are {age} years old.");
+                    else
+                        Console.WriteLine("your gender is not Male(M).");
+                
+            }
+            catch (Exception ex)
+            {
+                //Log to DB
+                //Log to File
+                //Notify Admin
+                Console.WriteLine($"Error: {ex.Message}");
+                throw;
+
+            }
         }
     }
 }
